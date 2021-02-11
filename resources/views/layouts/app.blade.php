54ec38127c4c785288c64f5ca65563002e13e92b
Wawingi António <wantonio@KC-LTOP-DSO-5.kixicredito.net>
2021-02-11 16:30:32 +0100

    Actulaização do reload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta http-equiv="refresh" content="30">
+    <!--<meta http-equiv="refresh" content="30">-->
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
